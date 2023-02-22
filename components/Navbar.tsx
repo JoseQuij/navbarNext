16110a76bb002b098f372d20d2b9bff07fced5b5
Jose Quijije <josequijije5@gmail.com>
2023-02-22 17:40:17 -0600

    done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,8 @@
 "use client"
+
 import React, { useState} from 'react'
 import { useTheme } from 'next-themes'
-import Link from 'next/link'
+import { Link } from 'react-scroll'
 import { RiMoonFill, RiSunLine } from 'react-icons/ri'
 import { IoMdMenu, IoMdClose } from 'react-icons/io'
 
@@ -31,29 +32,29 @@ const NavBar = () => {
     const currentTheme = theme === "system" ? systemTheme : theme
     const [navbar, setNavBar] = useState(false) 
   return (
-    <header className='w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-blue-700  dark:border-b dark:border-stone-600 dark:bg-black'>
-        <div className=' justify-between md:items-center md:flex '>
+    <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow dark:bg-stone-900 dark:border-b dark:border-stone-600">
+        <div className=' justify-between md:items-center md:flex'>
             <div>
                 <div className='flex items-center justify-between py-3'>
                     <div className='md:py-5 md:block'>
-                        <h2 className='text-white text-2xl font-bold'>Jose Quijije</h2>
+                        <h2 className=' text-2xl font-bold'>Jose Quijije</h2>
                     </div>
-                    <div className='md:hidden'>
-                        <button className='text-bold text-white' onClick={() =>setNavBar(!navbar)} >
+                    <div className='md:hidden '>
+                        <button className='p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border ' onClick={() =>setNavBar(!navbar)} >
                             {navbar ? <IoMdClose size={30}/> : <IoMdMenu size={30}/>}
                         </button>
                     </div>
                 </div>  
             </div>
-            <div className=''>
+            <div >
                 <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${ navbar ? "block" : "hidden"}`} >
-                    <div className='items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0'>
+                    <div className=' items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0 backdrop-blur-lg md:backdrop-blur-0'>
                         {Nav_Items.map((item, idx) =>{
                             return <Link 
                                key={idx}
-                               href={item.page}
+                               to={item.page}
                                className={
-                                "block lg:inline-block text-neutral-900 hover:text-neutral-900 dark:text-neutral-100"
+                                "block lg:inline-block dark:text-slate-100 hover:scale-105 cursor-pointer "
                                }
                                
                                spy={true}
